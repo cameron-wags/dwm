d4a7f340afaf2e3ca10a033299e07bee95289997
Cameron Wagstaff <cam.wagstaff@gmail.com>
2021-05-25 07:59:45 -0400

    add client swapping patch

--- config.h
+++ config.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include "movestack.c"
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -81,6 +82,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,         togglebar,      {0} },
 	{ MODKEY,                       XK_j,         focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,         focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,         movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,         movestack,      {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_h,         incnmaster,     {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_l,         incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,         setmfact,       {.f = -0.025} },
