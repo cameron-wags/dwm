12f49d8bf699f4d0f4e546e11f8df3289ddfc5ef
Cameron Wagstaff <cam.wagstaff@gmail.com>
2021-09-02 19:57:28 -0400

    mpv is floating

--- config.h
+++ config.h
@@ -40,6 +40,7 @@ static const Rule rules[] = {
 	{ "Gimp",               NULL,       NULL,       0,            0,           -1 },
 	{ "Firefox",            NULL,       NULL,       0,            0,           -1 },
 	{ "code",               NULL,       NULL,       0,            0,           -1 },
+	{ "mpv",                NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
